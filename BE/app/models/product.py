c86523f7689822bb92b11cf8d16d3969c31c490b
Tinhvila <ptinh4225@gmail.com>
2025-03-30 15:55:39 +0700

    init backend

---
+++ BE/app/models/product.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Text, Numeric, Boolean, DateTime, func
+from app.database import Base
+
+class ProductModel(Base):
+    __tablename__ = "products"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), nullable=False)
+    description = Column(Text, nullable=True)
+    price = Column(Numeric(10, 2), nullable=False)
+    image_url = Column(String(255), nullable=True)
+    is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
